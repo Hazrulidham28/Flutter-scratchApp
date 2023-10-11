219b07b0a5a090ee169b24a2ead172a2fae7d784
Hazrul <hazrulidham28@gmail.com>
2023-10-12 07:04:23 +0800

    complete version of quiz apps

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText = 'You Did It!';
@@ -23,9 +24,21 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+          TextButton(
+            onPressed: resetHandler,
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.blue)),
+            child: Text(
+              'Restart Quiz',
+            ),
+          ),
+        ],
       ),
     );
   }
